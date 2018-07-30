ac1e8d87dadf66d00b05f3cb41b3f1b601ee7d34
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-07-31 02:09:12 +0800

    Listen for enter key press

--- spoiler.js
+++ spoiler.js
@@ -1,15 +1,15 @@
 
-var spoilerItemsList;
+ var spoilerItemsList = [];
 
-chrome.storage.sync.get("spoiler", function(res) {
-	spoilerItemsList = res;
-	if (spoilerItemsList['spoiler'] == null) {
-		spoilerItemsList = {
-			'spoiler': [] //initialize spoilerItemsList object
-		};
-		saveSpoilerList();
-	}
-});
+// chrome.storage.sync.get("spoiler", function(res) {
+// 	spoilerItemsList = res;
+// 	if (spoilerItemsList['spoiler'] == null) {
+// 		spoilerItemsList = {
+// 			'spoiler': [] //initialize spoilerItemsList object
+// 		};
+// 		saveSpoilerList();
+// 	}
+// });
 
 saveSpoilerList = function() {
 	chrome.storage.sync.set({
@@ -25,22 +25,29 @@ window.onload = runOnPageLoad;
 
 function runOnPageLoad() {
 	console.log('works');
-	updateList();
-	searchPageForSpoilers();
-	var submitButton = document.getElementById("Submit_button");
+	//updateList();
+	//searchPageForSpoilers();
+	var submitButton = document.getElementById("enter");
 	var input = document.getElementById("blocker");
 	submitButton.addEventListener("click", getSpoilerText);
+	input.addEventListener("keypress", getSpoilerTextAfterKeyPress);
 
-	function getSpoilerText(event) {
+	function getSpoilerText() {
+		if (input.value.length > 0) {
+			createElement();
+		}
+	}
+	function getSpoilerTextAfterKeyPress(event) {
 		if (input.value.length > 0 && event.keyCode === 13) {
 			createElement();
 		}
 	}
 	function createElement() {
+		console.log(input.value);
 		spoilerItemsList.push(input.value);
-		saveSpoilerList();
+		//saveSpoilerList();
 		input.value = '';
-		updateList();
-		searchPageForSpoilers();
+		//updateList();
+		//searchPageForSpoilers();
 	}
 }

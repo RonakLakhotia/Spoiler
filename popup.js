988221a73a9187ea3313035015aedbfc8287411f
Ronak Lakhotia <ronak.lakhotia999@gmail.com>
2018-08-09 23:19:29 +0800

    Add background script and separate logic

---
+++ popup.js
@@ -0,0 +1,87 @@
+var spoilerItemsList ;
+var listOfItems = document.getElementById("list");
+
+   chrome.storage.sync.get("spoiler", function (results) {
+    spoilerItemsList = results;
+    updateList();	
+    if (spoilerItemsList['spoiler'] == null) {
+        spoilerItemsList = {
+            'spoiler': []
+        };
+        save();
+    }
+});
+ 
+
+   function save() {
+	chrome.storage.sync.set({
+		'spoiler': spoilerItemsList['spoiler']
+	}, function(result) {
+		if (chrome.runtime.error) {
+			console.log(chrome.runtime.error);
+		}
+	}); 
+
+}
+	document.addEventListener('click', function(event) {
+		var itemClicked = event.target;
+		if (itemClicked.className === 'spoilerListItem') {
+			itemClicked.parentNode.parentNode.removeChild(itemClicked.parentNode);
+			spoilerItemsList['spoiler'].splice(spoilerItemsList['spoiler'].indexOf(itemClicked.text), 1);
+		}
+	});
+
+	var submitButton = document.getElementById("enter");
+	var input = document.getElementById("blocker");
+	var clearButton = document.getElementById("clear-button");
+
+	clearButton.addEventListener("click", clearList);
+	submitButton.addEventListener("click", getSpoilerText);
+	input.addEventListener("keypress", getSpoilerTextAfterKeyPress);
+
+	function runPageSearch(type) {
+		save();
+		input.value = '';
+		updateList();
+		searchPageForSpoilers(type);
+	}
+	function getSpoilerText() {
+		if (input.value.length > 0) {
+			createElement();
+		}
+	}
+	function getSpoilerTextAfterKeyPress(event) {
+		if (input.value.length > 0 && event.keyCode === 13) {
+			createElement();
+		}
+	}
+	function clearList() {
+		let CLEAR = "clear";
+		spoilerItemsList = {
+			'spoiler': []
+		};
+		runPageSearch(CLEAR);
+	}
+	function createElement() {
+		spoilerItemsList['spoiler'].push(input.value);
+		runPageSearch(input.value);
+	}
+
+	function updateList() {
+	if (spoilerItemsList['spoiler'] != null) {
+		listOfItems.innerHTML = "";
+		var html = "<ul>";
+		for (let i = 0; i < spoilerItemsList['spoiler'].length; i++) {
+			html += '<li><a class="spoilerListItem" href="#">' + spoilerItemsList['spoiler'][i] + '</a></li>';
+		}
+		html += "</ul>";
+		listOfItems.innerHTML = html;
+	}
+}
+	function searchPageForSpoilers(type) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {action: type}, function(response) {});  
+		});
+	}
+
+
--- spoiler.js
+++ spoiler.js
@@ -1,97 +1,42 @@
+var spoilerItemsList ;
+var text;
+chrome.storage.sync.get("spoiler", function (results) {
+    spoilerItemsList = results;
+    if (spoilerItemsList['spoiler'] == null) {
+        spoilerItemsList = {
+            'spoiler': []
+        };
+        save();
+    }
+});
 
-var spoilerItemsList = {};
-var listOfItems = document.getElementById("list");
-// chrome.storage.sync.get("spoiler", function(res) {
-// 	spoilerItemsList = res;
+chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+
+  	text = msg.action;
 	if (spoilerItemsList['spoiler'] == null) {
-		spoilerItemsList = {
-			'spoiler': [] //initialize spoilerItemsList object
-		};
-//		saveSpoilerList();
-	}
-// });
-
-saveSpoilerList = function() {
-	chrome.storage.sync.set({
+	spoilerItemsList = {
+		'spoiler': [] //initialize spoilerItemsList object
+	};
+	save();
+}
+  if (text === 'clear') {
+	spoilerItemsList = {
+		'spoiler': []
+	};
+  } else {
+   spoilerItemsList['spoiler'].push(text);
+   console.log(text);
+  }
+   save();
+});
+   function save() {
+   		chrome.storage.sync.set({
 		'spoiler': spoilerItemsList['spoiler']
 	}, function(result) {
 		if (chrome.runtime.error) {
 			console.log(chrome.runtime.error);
 		}
-	});
+	}); 
+	alert(spoilerItemsList['spoiler']);
+
 }
-
-window.onload = runOnPageLoad;
-
-function runOnPageLoad() {
-	updateList();
-	//searchPageForSpoilers();
-
-	document.addEventListener('click', function(event) {
-		var itemClicked = event.target;
-		if (itemClicked.className === 'spoilerListItem') {
-			itemClicked.parentNode.parentNode.removeChild(itemClicked.parentNode);
-			spoilerItemsList['spoiler'].splice(spoilerItemsList['spoiler'].indexOf(itemClicked.text), 1);
-		}
-	});
-	var submitButton = document.getElementById("enter");
-	var input = document.getElementById("blocker");
-	var clearButton = document.getElementById("clear-button");
-
-	clearButton.addEventListener("click", clearList);
-	submitButton.addEventListener("click", getSpoilerText);
-	input.addEventListener("keypress", getSpoilerTextAfterKeyPress);
-
-	function runPageSearch() {
-		//saveSpoilerList();
-		input.value = '';
-		updateList();
-		//searchPageForSpoilers();
-	}
-	function getSpoilerText() {
-		if (input.value.length > 0) {
-			createElement();
-		}
-	}
-	function getSpoilerTextAfterKeyPress(event) {
-		if (input.value.length > 0 && event.keyCode === 13) {
-			createElement();
-		}
-	}
-	function clearList() {
-		spoilerItemsList = {
-			'spoiler': []
-		};console.log('works');
-		runPageSearch();
-	}
-	function createElement() {
-		spoilerItemsList['spoiler'].push(input.value);
-		console.log(spoilerItemsList);
-		runPageSearch();
-	}
-}
-
-function updateList() {
-	if (spoilerItemsList['spoiler'] != null) {
-		listOfItems.innerHTML = "";
-		var html = "<ul>";
-		for (let i = 0; i < spoilerItemsList['spoiler'].length; i++) {
-			html += '<li><a class="spoilerListItem" href="#">' + spoilerItemsList['spoiler'][i] + '</a></li>';
-		}
-		html += "</ul>";
-		listOfItems.innerHTML = html;
-	}
-}
-
-function saveSpoilerList() {
-	chrome.storage.sync.set({
-		'spoiler': spoilerItemsList['spoiler']
-	}, function(result) {
-		if (chrome.runtime.error) {
-			console.log(chrome.runtime.error);
-		}
-	});
-}
-
-
-
